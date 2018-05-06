4c4738fafc85063093cab9fdcabde8761048856d
Marcos Scorzoni <mlessa@gmail.com>
2018-05-06 18:39:46 -0300

    rspec user geocoded

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 describe User do 
  
   it 'has to give an address and a name' do
-    user3 = User.create(name: 'pat', address: 'adress', email: 'pat@example.com', password: '123456', password_confirmation: '123456')
+    user3 = User.create(name: 'pat', address: 'rua mourato coelho, 1404 - Sao Paulo, Brazil', email: 'pat@example.com', password: '123456', password_confirmation: '123456')
     user4 = User.create(email: 'patt@example.com', password: '123456', password_confirmation: '123456')
     expect(User.count).to eq(1)
+  end
+
+  it 'has to contain geocode' do
+    user3 = User.create(name: 'pat', address: 'rua mourato coelho, 1404 - Sao Paulo, Brazil', email: 'pat@example.com', password: '123456', password_confirmation: '123456')
+    expect(user3.longitude).to_not eq(nil)
   end
 end
